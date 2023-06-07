bc3998825670aa9f161ec64ec7658a47d4cc9fc0
junqwoopark <junqwoopark@gmail.com>
2023-06-07 15:01:25 +0900

    feat: getAccessToken() & getUserInfo() 구현
    Co-authored-by: 허민 (Her Min) <hermin9804@gmail.com>
    Co-authored-by: namzisun <namzisun@users.noreply.github.com>
    Co-authored-by: seunghye218 <seunghye218@users.noreply.github.com>
    Co-authored-by: jrim <jrim42@users.noreply.github.com>

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,19 +5,37 @@ import axios from 'axios';
 @Injectable()
 export class AuthService {
   async postAuthTo42(authDto: AuthDto) {
-    console.log('authDto.code: ', authDto.code);
-    console.log(process.env.FT_CLIENT_ID);
-    try {
-      const res = await axios.post('https://api.intra.42.fr/oauth/token', {
-        grant_type: 'authorization_code',
-        client_id: process.env.FT_CLIENT_ID,
-        client_secret: process.env.FT_CLIENT_SECRET,
-        code: authDto.code,
-        redirect_uri: 'http://127.0.0.1:3000/auth',
-      });
-      console.log('res: ', res);
-    } catch (err) {
-      console.log('err: ', err);
-    }
+    const getAccessToken = async () => {
+      try {
+        const res = await axios.post('https://api.intra.42.fr/oauth/token', {
+          grant_type: 'authorization_code',
+          client_id: process.env.FT_CLIENT_ID,
+          client_secret: process.env.FT_CLIENT_SECRET,
+          code: authDto.code,
+          redirect_uri: 'http://127.0.0.1:3000/auth',
+        });
+        return res.data.access_token;
+      } catch (err) {
+        console.log('* err: getAccessToken: ', err.response.data);
+      }
+    };
+
+    const getUserInfo = async () => {
+      try {
+        const res = await axios.get(
+          `https://api.intra.42.fr/v2/me?access_token=${accessToken}`,
+        );
+        return res.data;
+      } catch (err) {
+        console.log('* err: getUserInfo: ', err.response.data);
+      }
+    };
+
+    const accessToken = await getAccessToken();
+    // console.log(accessToken);
+
+    const userInfo = await getUserInfo();
+    const { id, email, login, displayname, image } = userInfo;
+    console.log(id, email, login, displayname, image);
   }
 }
