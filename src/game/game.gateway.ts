c3417d167b1c96dc40532f8cebe8d949f5088c18
Muser218 <aogidwndeld@naver.com>
2023-07-10 18:17:50 +0900

    feat: matchMaking 및 소켓 jwt 검증
    Co-authored-by: junqwoopark <junqwoopark@users.noreply.github.com>

--- src/game/game.gateway.ts
+++ src/game/game.gateway.ts
@@ -20,6 +20,7 @@ import {
   PLANE_WIDTH,
   PLANE_HEIGHT,
 } from './game.constants';
+import { JwtService } from '@nestjs/jwt';
 
 @WebSocketGateway(4242, {
   namespace: 'game',
@@ -36,52 +37,59 @@ import {
 export class GameGateway
   implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
 {
+  private waitingQueue: Socket[] = [];
+
   @WebSocketServer() io: Server;
   private logger = new Logger('GameGateway');
 
-  constructor(private gameEngine: GameEngine) {
+  constructor(private gameEngine: GameEngine, private jwtService: JwtService) {
     this.logger.log('GameGateway constructor');
   }
 
   afterInit(server: any) {
     this.logger.log('GameGateway initialized');
+
+    const interval = setInterval(() => {
+      console.log('waitingQueue.length: ', this.waitingQueue.length);
+
+      if (this.waitingQueue.length >= 2) {
+        const player1 = this.waitingQueue.shift();
+        const player2 = this.waitingQueue.shift();
+        player1.emit('match_found', 'Match found');
+        player2.emit('match_found', 'Match found');
+        player1.join('dummy_room');
+        player2.join('dummy_room');
+        const room = this.io.in('dummy_room');
+        room['player1'] = player1;
+        room['player2'] = player2;
+
+        this.gameEngine.gameInit(room);
+        this.gameEngine.gameLoop(room);
+      }
+    }, 5000);
   }
 
-  async handleConnection(@ConnectedSocket() socket: Socket) {
+  // jwt token 검증
+  handleConnection(@ConnectedSocket() socket: Socket) {
     this.logger.log(`Client connected: ${socket.id}`);
+    const accessToken = socket.handshake.query.accessToken as string;
 
-    socket.join('dummy_room');
-    console.log('socket.rooms: ', socket.rooms);
-
-    // check two player
-    const clientsInRoom = await this.io.in('dummy_room').fetchSockets();
-    //console.log(clientsInRoom);
-    if (clientsInRoom.length === 2) {
-      const player1 = clientsInRoom[0];
-      const player2 = clientsInRoom[1];
-
-      const room = this.io.in('dummy_room');
-      this.io.in('dummy_room').emit('game_test', 'Game start');
-      room['player1'] = player1;
-      room['player2'] = player2;
-      this.gameEngine.gameInit(room);
-      this.gameEngine.gameLoop(room);
-
-      console.log('player1: ', player1['paddle1']);
-      // this.gameEngine.gameLoop(socket);
-    } else {
-      socket.emit('game_test', 'Waiting for another player');
+    try {
+      const decoded = this.jwtService.verify(accessToken);
+      socket['ftId'] = decoded['sub'];
+    } catch (error) {
+      socket.disconnect();
     }
   }
 
   handleDisconnect(@ConnectedSocket() socket: Socket) {
     this.logger.log(`Client disconnected: ${socket.id}`);
-    // TODO interval 삭제
-    //const interval = socket['interval'];
-    const interval = this.io.in('dummy_room')['interval'];
-    console.log('interval: ', interval);
     socket.leave('dummy_room');
-    clearInterval(interval);
+  }
+
+  @SubscribeMessage('start_matchmaking')
+  handleStartMatchmaking(@ConnectedSocket() socket: Socket) {
+    this.waitingQueue.push(socket);
   }
 
   @SubscribeMessage('key_left')
